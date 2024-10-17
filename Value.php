ebd6a8e43a55d92e1d261ba45c11fd55a82a2f69
Oran Fry <oran@emerg.co>
2024-10-17 17:22:58 +1300

    Values
    Add nullable property, defaulting to true (status quo).

--- Value.php
+++ Value.php
@@ -6,16 +6,26 @@ class Value extends ContextVariableSet
 {
     public $options;
     public $value;
+    public bool $nullable;
 
     public function __construct(string $prefix, array $default_data = [], ?string $partial = null)
     {
+        $this->nullable = (bool) ($default_data['nullable'] ?? true);
         $this->options = $default_data['options'] ?? null;
 
-        unset($default_data['options']);
+        unset($default_data['nullable'], $default_data['options']);
+
+        if (!$this->nullable && $this->options) {
+            $default_data['value'] ??= reset($this->options);
+        }
 
         parent::__construct($prefix, $default_data, $partial);
 
         $this->value = @$this->getRawData()['value'];
+
+        if (!$this->nullable && !$this->value) {
+            throw new Exception('Value [' . $prefix . '] is not nullable but no value could be established. Suggestion: provide a default value.');
+        }
     }
 
     public function input_names(): array
--- src/php/partial/ContextVariableSets/Value.php
+++ src/php/partial/ContextVariableSets/Value.php
@@ -2,37 +2,41 @@
 
 $label = $this->value;
 
-if ($this->options):
+if ($this->options) {
     $option_lookup = array_flip($this->options);
 
-    if (isset($option_lookup[$this->value]) && is_string($option_lookup[$this->value])) :
+    if (
+        isset($option_lookup[$this->value])
+        && is_string($option_lookup[$this->value])
+    ) {
         $label = $option_lookup[$this->value];
-    endif;
-endif;
+    }
+}
 
 $label = $label ?: $this->label ?: preg_replace('/.*_/', '', $this->prefix);
 
-?><div class="navset">
-    <div class="inline-rel">
-        <div class="inline-modal">
-            <div class="inline-dropdown">
-                <a class="cv-manip<?= $this->value ? null : ' current' ?><?= $this->disabled ? ' cv-disabled' : null ?>" data-manips="<?= $this->prefix ?>__value=">-</a>
-                <?php
-                    if ($this->options):
-                        foreach ($this->options as $index => $option):
-                            $current = $this->value == $option ? 'current' : '';
-                            $manips = $this->prefix . '__value=' . $option;
+?><div class="navset"><?php
+    ?><div class="inline-rel"><?php
+        ?><div class="inline-modal"><?php
+            ?><div class="inline-dropdown"><?php
+                if ($this->nullable) {
+                    ?><a class="cv-manip<?= $this->value ? null : ' current' ?><?= $this->disabled ? ' cv-disabled' : null ?>" data-manips="<?= $this->prefix ?>__value=">-</a><?php
+                }
 
-                            ?><a class="cv-manip <?= $current ?><?= $this->disabled ? ' cv-disabled' : null ?>" data-manips="<?= $manips ?>"><?php
-                                echo !is_numeric($index) ? $index : $option;
-                            ?></a><?php
-                        endforeach;
-                     else:
-                        ?><input class="cv-surrogate<?= $this->disabled ? ' cv-disabled' : null ?>" data-for="<?= $this->prefix ?>__value" type="text" value="<?= $this->value ?>"><?php
-                    endif;
-                ?>
-            </div>
-        </div>
-        <span class="inline-modal-trigger <?= $this->disabled ? ' cv-disabled' : null ?>"><?= $label ?></span>
-    </div>
-</div>
+                if ($this->options) {
+                    foreach ($this->options as $index => $option) {
+                        $current = $this->value == $option ? 'current' : '';
+                        $manips = $this->prefix . '__value=' . $option;
+
+                        ?><a class="cv-manip <?= $current ?><?= $this->disabled ? ' cv-disabled' : null ?>" data-manips="<?= $manips ?>"><?php
+                            echo !is_numeric($index) ? $index : $option;
+                        ?></a><?php
+                    }
+                 } else {
+                    ?><input class="cv-surrogate<?= $this->disabled ? ' cv-disabled' : null ?>" data-for="<?= $this->prefix ?>__value" type="text" value="<?= $this->value ?>"><?php
+                }
+            ?></div><?php
+        ?></div><?php
+        ?><span class="inline-modal-trigger <?= $this->disabled ? ' cv-disabled' : null ?>"><?= $label ?></span><?php
+    ?></div><?php
+?></div><?php
